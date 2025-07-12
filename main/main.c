83c20ea1fcc1a430552f198bda6444744cb12559
1440 <mew@localhost.localdomain>
2025-07-12 07:35:26 -0400

    Call display_detect() and display_init() in main startup

--- main/main.c
+++ main/main.c
@@ -1,3 +1,32 @@
+#include "display.h"
+
+void app_main(void)
+{
+    usb_host_init();
+
+    if (display_detect()) {
+        display_init();
+        display_write_line(0, "PadBeacon boot");
+    }
+
+    ble_init();
+
+    while (1) {
+        gamepad_state_t st = {0};
+        usb_poll(&st);
+
+        if (st.connected) {
+            ble_send_gamepad_report(&st);
+        }
+
+        if (st.connected && s_present) {
+            display_write_line(1, "USB OK");
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(16));
+    }
+}
+
 #include "usb_host.h"
 #include "ble_output.h"
 #include "gamepad.h"
